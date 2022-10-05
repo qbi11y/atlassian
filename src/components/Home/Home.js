25a35b7b21e8290e9eff33d5d460e7e6c281e391
James Thompson <qbi11y@gmail.com>
2022-10-05 18:08:41 -0500

    inital project setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import logo from '../../images/horizontal-logo-gradient-blue-atlassian.png';
+
+const Home = () => {
+    return(
+        <div>
+            <img src={logo} alt="logo" />
+        </div>
+        
+    )
+}
+
+export default Home
