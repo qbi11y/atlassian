a7cd552ede53d41544b2b8a35c6f8fc613e73356
James Thompson <qbi11y@gmail.com>
2022-10-05 22:39:45 -0500

    additional setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import logo from '../../images/horizontal-logo-gradient-blue-atlassian.png';
+import './_home.scss';
+
 
 const Home = () => {
+    const [ someState, setSomeState ] = useState()
     return(
         <div>
             <img src={logo} alt="logo" />
+            <p className="copy">Text here</p>
+            <div className="content">
+                <section className="section">Column 1</section>
+                <section className="section">Column 2</section>
+                <section className="section">Column 3</section>
+            </div>
         </div>
         
     )
